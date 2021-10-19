15e87c07b2eb4343e6a1403464aa90b00158589b
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-10-19 09:26:34 -0400

    So far so good

--- src/map/scroll-indicator/index.jsx
+++ src/map/scroll-indicator/index.jsx
@@ -1,31 +1,38 @@
 import './scroll-indicator.css';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import PlusSign from './plus-sign';
+import MinusSign from './minus-sign';
 
 function ScrollIndicator({ mapBoxContainerRef, valueUpdate }) {
+    const ballRef = useRef(null);
     const minVal = 0;
     const maxVal = 100;
     
     useEffect(() => {
         valueUpdate(maxVal);
         let value = maxVal;
+        ballRef.current.setAttribute('style', `right: ${value}%`);
+
         mapBoxContainerRef.current.addEventListener('wheel', (e) => {
             const delta = parseInt(Math.sign(e.deltaY));
             if (value + delta < maxVal + 1 && delta > 0) {
                 value += delta;
                 valueUpdate(value);
+                ballRef.current.setAttribute('style', `right: ${value}%`);
             } else if (value + delta > minVal - 1 && delta < 0) {
                 value += delta;
                 valueUpdate(value);
+                ballRef.current.setAttribute('style', `right: ${value}%`);
             }
         });
     }, [mapBoxContainerRef, valueUpdate])
 
     return (
         <div className="scroll-indicator">
-            {/* This is needed to allow relative position inside scroll indicator */}
+        {/* This is needed to allow relative position inside scroll indicator */}
             <div className="scroll-container">
                 <div className="line"></div>
-                <div className="ball"></div>
+                <div className="ball" ref={ballRef}></div>
             </div>
         </div>
     );
