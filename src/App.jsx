fcfd217e334627eefceeb5f89d77cd9e18eec72a
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-10-21 20:43:10 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Map from "./map";
+import Nav from './nav';
 
 function App() {
   return (
     <div className="App">
+        <Nav />
         <Map />
     </div>
   );
