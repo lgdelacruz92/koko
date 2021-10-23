119b6883215268c421b2c02d6c7e224d655ca03a
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-10-23 10:47:16 -0400

    Good start

--- src/side-panel/index.jsx
+++ src/side-panel/index.jsx
@@ -1,8 +1,8 @@
 import './side-panel.css';
+import Feature from './feature';
 
 export default function SidePanel({ show }) {
-
-    // Kinda shitty. I can probably manually build something better.
-    return <div className={"side-panel" + (show ? ' show-side-panel' : '')}> 
+    return <div className={"side-panel" + (show ? ' show-side-panel' : '')}>
+        <Feature />
     </div>
 }
