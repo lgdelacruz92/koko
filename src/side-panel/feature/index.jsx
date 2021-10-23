68161617d495676d1dc5416abf141a578a898893
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-10-23 11:28:03 -0400

    So far so good

--- src/side-panel/feature/index.jsx
+++ src/side-panel/feature/index.jsx
@@ -1,3 +1,10 @@
-export default function Feature() {
-    return <div>hello</div>
+import './feature.css';
+
+export default function Feature({ title, description }) {
+
+    return <div className="feature">
+        <div><span className="title">{title}</span></div>
+        <div><span className="description">{description}</span></div>
+    </div>
+
 }
--- src/side-panel/index.jsx
+++ src/side-panel/index.jsx
@@ -1,8 +1,31 @@
 import './side-panel.css';
 import Feature from './feature';
+import { useRef, useEffect } from 'react';
 
 export default function SidePanel({ show }) {
-    return <div className={"side-panel" + (show ? ' show-side-panel' : '')}>
-        <Feature />
+    const sidePanelRef = useRef(null);
+    const sidePanelContainerRef = useRef(null);
+
+    useEffect(() => {
+        const sidePanelChanged = e => {
+            const sidePanel = e.filter(observed => {
+                return observed.target === sidePanelRef.current
+            })[0];
+            if (sidePanel.contentRect['width'] > 250) {
+                sidePanelContainerRef.current.setAttribute('style', '');
+            } else {
+                sidePanelContainerRef.current.setAttribute('style', 'display: none');
+            }
+        }
+        new ResizeObserver(sidePanelChanged).observe(sidePanelRef.current);
+    }, []);
+
+    return <div className={"side-panel" + (show ? ' show-side-panel' : '')} ref={sidePanelRef}>
+        <div ref={sidePanelContainerRef}>
+            <Feature 
+                title="20 to 24 Year Olds in Florida Per County"
+                description="The number of people between the age of 20 to 24 in Florida."
+            />
+        </div>
     </div>
 }
