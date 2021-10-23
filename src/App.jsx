f574e6a2d2e3a3e8f781984568f44139dad773c5
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-10-23 07:43:02 -0400

    Functional input box but still kinda shitty

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import "./App.css";
 import Map from "./map";
 import Nav from './nav';
+import SidePanel from './side-panel';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-        <div className="main-panel">
-            <Nav />
-            <Map />
-            <div className="state-info-popup">
-                <div id="state">CA</div>
-                <div id="county">Santa Barbara</div>
-                <div id="value">Value</div>
+    const [showSidePanel, setShowSidePanel] = useState(false);
+
+    const hamburgerClick = () => {
+        setShowSidePanel(!showSidePanel);
+    }
+
+    return (
+        <div className="App">
+            <SidePanel show={showSidePanel} />
+            <div className="main-panel">
+                <Nav hamburgerClick={hamburgerClick}/>
+                <Map />
+                <div className="state-info-popup">
+                    <div id="state">CA</div>
+                    <div id="county">Santa Barbara</div>
+                    <div id="value">Value</div>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
