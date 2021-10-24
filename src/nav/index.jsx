8a134a955fce32c2ba2fb49b96748c1c3cb9ec40
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-10-24 18:39:02 -0400

    nav bar hamburger

--- src/nav/index.jsx
+++ src/nav/index.jsx
@@ -19,9 +19,11 @@ export default function Nav({ hamburgerClick, searchAction }) {
     }, [searchAction]);
 
     return <div className="navbar">
-        <button onClick={hamburgerClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
-        </button>
+        <div>
+            <button className="hamburger" onClick={hamburgerClick}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
+            </button>
+        </div>
         <div className="search-input">
             {/* magnifying class icon */}
             <span className="icon" onClick={() => {
