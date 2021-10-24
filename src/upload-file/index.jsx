cf339c8ef556ba9884f2dd10e036ce28ee1e70a9
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-10-24 19:25:19 -0400

    work-flow stuff

--- src/upload-file/index.jsx
+++ src/upload-file/index.jsx
@@ -1,18 +1,36 @@
 import './upload-file.css';
 import { Paper, Button } from '@mui/material';
+import { useState } from 'react';
 
 
 export default function UploadFile() {
+    const [workflowIndex, setWorkflowIndex] = useState(0);
+    const workflowPicker = index => {
+        if (index === 0) {
+            return <div>work-flow 1</div>;
+        } else if (index === 1) {
+            return <div>work-flow 2</div>;
+        } else if (index === 2) {
+            return <div>work-flow 3</div>
+        } else {
+            return <div>work-flow 4</div>
+        }
+    }
+
     return <Paper className="upload-file-dialog" elevation={3}>
         <div className="workflow">
-            <div className="workflow-item">
-                <div className="content">workflow 1</div>
-            </div>
+            {workflowPicker(workflowIndex)}
         </div>
         <div className="actions">
-            <span><Button>Back</Button></span>
             <span><Button onClick={() => {
-
+                if (workflowIndex - 1 >= 0) {
+                    setWorkflowIndex(workflowIndex - 1);
+                }
+            }}>Back</Button></span>
+            <span><Button onClick={() => {
+                if (workflowIndex + 1 < 4) {
+                    setWorkflowIndex(workflowIndex + 1);
+                }
             }}>Next</Button></span>
         </div>
     </Paper>;
